bb3e60fdde5803a704c730e6395c558bc6b459f9
Issa Daou <idaou-24@tp-1a201-19.enst.fr>
2025-07-14 03:41:32 +0200

    sort

--- seive.c
+++ seive.c
@@ -3,25 +3,35 @@
 #include<stdbool.h>
 
 static bool nbre[100];
+
+
+
+void marquer_multiple(int number){
+    for(int i=2*number;i<100;i=i+number){
+        nbre[i]=false; 
+    }
+} 
+
 void sieve(){
     for(int i=0;i<100;i++){
         nbre[i]=true;
+    } 
 
-        for(int j=2;j<100;j++){
-            if(j%i==0){
-                nbre[j]=false;
-            }
-        }
+     for(int i=2;i<100;i++){
+        marquer_multiple(i);
+     }   
 
-    }
+    
 }
 
 int main(){
+    
     sieve();
-    for(int i=0; i<100;i++){
+    for(int i=2; i<100;i++){
         if(nbre[i]){
-            printf("%d",i);
+            printf("%d\n ",i);
         }
     }
+    
     return EXIT_SUCCESS;
 }
